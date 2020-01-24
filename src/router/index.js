4c1e366afd4cdb2f1310f9b55a1d10fdeab177a0
zhangyuchi <2669327287@qq.com>
2020-01-24 19:52:28 +0800

    添加根路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,22 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path:"/",
+    redirect:"/login"
+  },
+  {
+    path:"/login",
+    component:()=>import('../views/login.vue')
+  },
+  {
+    path:"/member",
+    component:()=>import('../views/member/index.vue')
+  },
+  {
+    path:"/organizer",
+    component:()=>import('../views/organizer/index.vue')
+  }
 ]
 
 const router = new VueRouter({
