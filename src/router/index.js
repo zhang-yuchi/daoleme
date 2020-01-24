a6e30a7d4061946fd6f2a58dbdbf5e179958ec5d
zhangyuchi <2669327287@qq.com>
2020-01-24 22:20:46 +0800

    添加成员和管理员相关路由

--- src/router/index.js
+++ src/router/index.js
@@ -15,16 +15,44 @@ const routes = [
   },
   {
     path:"/member",
-    component:()=>import('../views/member/index.vue')
+    component:()=>import('../views/member/index.vue'),//放导航
+    children:[
+      {
+        path:"action",
+        component:()=>import('../views/member/action/action.vue')
+      },
+      {
+        path:"profile",
+        component:()=>import('../views/member/profile/profile.vue')
+      }
+    ]
   },
   {
     path:"/organizer",
-    component:()=>import('../views/organizer/index.vue')
+    component:()=>import('../views/organizer/index.vue'),
+    children:[
+      {
+        path:'action',
+        component:()=>import('../views/organizer/action/action.vue')
+      },
+      {
+        path:'profile',
+        component:()=>import('../views/organizer/profile/profile.vue')
+      },
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to,from,next)=>{
+  console.log(to)
+  console.log("this is from")
+  console.log(from)
+  console.log("___")
+  if(from.meta.identify=="member"){
+    console.log("是成员进入")
+  }
+})
 export default router
